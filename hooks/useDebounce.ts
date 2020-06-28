202235a587f20b6e1651999038d01931a08fb1cf
khaled <kzack-dev@hotmail.com>
2020-06-28 04:55:23 +0300

    add commonly used hooks

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export default function useDebounce<T>(value: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(
+    () => {
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+
+  return debouncedValue;
+}
---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, useRef } from 'react';
+
+export default function useLocalStorage<T>(key: string, defaultValue: T): [T, (newValue: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(defaultValue);
+  const defaultValueRef = useRef(defaultValue);
+
+  useEffect(() => {
+    try {
+      const item = localStorage.getItem(key);
+      const parsedItem = typeof item == 'string' ? JSON.parse(item) : defaultValueRef.current;
+      setStoredValue(parsedItem);
+    } catch (e) {
+      console.log('cannot read item from localStorage, fallback on default value: ', e);
+    }
+  }, [key]);
+
+  const setValue = (newValue: T): void => {
+    try {
+      setStoredValue(newValue);
+      localStorage.setItem(key, JSON.stringify(newValue));
+    } catch (e) {
+      console.log('cannot store value in localStorage: ', e);
+    }
+  };
+
+  return [storedValue, setValue];
+}
