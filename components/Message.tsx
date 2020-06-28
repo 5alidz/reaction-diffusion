00dcd16232b024e562fc766b6125e70eef5ea8a9
khaled <kzack-dev@hotmail.com>
2020-06-28 03:59:28 +0300

    change default msg

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,9 +1,9 @@
 import { motion } from 'framer-motion';
 
-export default function Message() {
+export default function Message({ children }: { children: string }) {
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-      <pre className='w-screen h-screen flex items-center justify-center'>Just Do It.</pre>
+      <pre className='w-screen h-screen flex items-center justify-center'>{children}</pre>
     </motion.div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Message from 'components/Message';
 export default function Home() {
   return (
     <main>
-      <Message />
+      <Message>Ok, Now what.</Message>
     </main>
   );
 }
