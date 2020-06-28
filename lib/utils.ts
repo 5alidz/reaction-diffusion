ffefd142175b5ec51d49777a80ec265bf6dcb152
khaled <kzack-dev@hotmail.com>
2020-06-28 03:10:43 +0300

    add cls utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,19 @@
 export const noop = () => {};
+
+export function cls(...classesToMerge: (null | undefined | string | [boolean, string, string?])[]): string {
+  const classes: string[] = [];
+  for (let i = 0; i < classesToMerge.length; i++) {
+    const current = classesToMerge[i];
+    if (Array.isArray(current)) {
+      const [condition, className, fallbackClassName] = current;
+      if (condition && className && typeof className === 'string') {
+        classes.push(className.trim());
+      } else if (!condition && fallbackClassName && typeof fallbackClassName === 'string') {
+        classes.push(fallbackClassName.trim());
+      }
+    } else if (typeof current === 'string' && current.length > 0) {
+      classes.push(current.trim());
+    }
+  }
+  return classes.join(' ');
+}
